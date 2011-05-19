f3b41db438adda894c86a1dd0f40faa49bb358bb
Zander <zander@martineau.tv>
2011-05-19 07:07:14 +0100

    - Change <title> to 'Boost' - Fix broken forms.css src

---
+++ js/script.js
@@ -0,0 +1,32 @@
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
+if (viewportmeta) {
+viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+document.body.addEventListener('gesturestart', function() {
+viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+}, false);
+}
+}
+
+/* Declare a namespace for the site */
+var Site = window.Site || {};
+
+/* Create a closure to maintain scope of the '$'
+   and remain compatible with other frameworks.  */
+(function($) {
+	
+	//same as $(document).ready();
+	$(function() {
+		
+		
+
+	});
+
+
+	$(window).bind("load", function() {
+		
+		
+	
+	});
+	
+})(jQuery);
