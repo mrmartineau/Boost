86f87bfbd6195f25c6ed17c0e2a254ef8879813a
Zander Martineau <zander@martineau.tv>
2011-07-09 12:58:16 +0100

    Add tabs script from Skeleton & also included some ARIA sauce

--- js/script.js
+++ js/script.js
@@ -18,9 +18,61 @@ var Site = window.Site || {};
 	//same as $(document).ready();
 	$(function() {
 		
+		// TABS from Skeleton (with my own ARIA sauce added)
+		// 	<ul class="group tabs">
+		// 		<li>
+		// 			<a href="#tab_1" class="active">Tab 1</a>
+		// 		</li>
+		// 		<li>
+		// 			<a href="#tab_2">Tab 2</a>
+		// 		</li>
+		// 	</ul>
+		// 	<ul class="tabs_content ui_list">
+		// 		<li id="tab_1" class="active">
+		// 			<h3>Tab 1 content</h3>
+		// 			<p>Pellentesque</p>
+		// 		</li>
+		// 		<li id="tab_2">
+		// 			<h3>Tab 2 content</h3>
+		// 			<p>Lorem ipsum</li>
+		// 	</ul>
+		var tabs = $('ul.tabs');
+
+		tabs.each(function (i) {
+			//Get all tabs
+			var tab = $(this).find('> li > a');
+			tab.each(function (i) {
+				var tabid = $(this).attr('href');
+				if (tabid.charAt(0) === '#') { newTabid = tabid.slice( 1 ); };
+				
+				$('#' + newTabid).each(function (i) {
+					$(this).attr('aria-labelledby', newTabid + '_tab');
+				});
+				
+				$(this).attr({
+					role: 'tab',
+					id: newTabid + '_tab'
+				}).attr('aria-describedby', newTabid);
+				
+			}).click(function (e) {
+				var contentLocation = $(this).attr('href');
+				if (contentLocation.charAt(0) === "#") {
+					e.preventDefault();
+					tab.removeClass('active');
+					$(this).addClass('active');
+					$(contentLocation).show()
+						.attr('role', 'aria-selected')
+						.addClass('active')
+						.siblings()
+						.attr('role', 'aria-hidden')
+						.hide()
+						.removeClass('active');
+				}
+			});
+		}); //END TABS
 		
 
-	});
+	});//End Doc Ready
 
 
 	$(window).bind("load", function() {
